55b07c59621f76cb31685ec5784ba592b859ee1a
Kieran Schubert <schubert.kieran@gmail.com>
2020-07-26 16:45:35 +0200

    extended the image url downloader to get all available google images

--- execute_js.py
+++ execute_js.py
@@ -1,22 +1,44 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementNotInteractableException
+
 import time
 
-def execute_js(url):
+def execute_js(url, mtn):
 	driver = webdriver.Chrome()
 
 	script = "urls=Array.from(document.querySelectorAll('.rg_i')).map(el=> el.hasAttribute('data-src')?el.getAttribute('data-src'):el.getAttribute('data-iurl'));window.open('data:text/csv;charset=utf-8,' + escape(urls.join('" + "\\" + "n')));"
 
-	#url = "https://www.google.com/search?tbm=isch&sxsrf=ALeKk03MLv9Cte7kUKbehP9fuVW93tFd1w%3A1595765306373&source=hp&biw=1280&bih=630&ei=OnIdX7vwFNiEk74Pz7WLoAo&q=matterhorn&oq=matterhorn&gs_lcp=CgNpbWcQA1CMAljCDWCOD2gAcAB4AIABAIgBAJIBAJgBAKABAaoBC2d3cy13aXotaW1n&sclient=img&ved=0ahUKEwi79eqB8erqAhVYwsQBHc_aAqQQ4dUDCAc&uact=5"
-
 	driver.get(url)
+
+	search = driver.find_element_by_name('q')
+	search.send_keys(mtn + " mountain")
+	search.send_keys(Keys.RETURN)
 
 	for i in range(5):
 		driver.execute_script("window.scrollTo(0, 40000);")
-		time.sleep(3)
+		time.sleep(1.5)
+
+	time.sleep(3)
+
+	while True:
+		try:
+			driver.find_element_by_xpath("//div[@class='YstHxe']//input[@type='button']").click()
+			time.sleep(1.5)
+			for i in range(5):
+				driver.execute_script("window.scrollTo(0, 80000);")
+				time.sleep(1.5)
+		except ElementNotInteractableException:
+			break
 
 	driver.execute_script(script)
 
 	time.sleep(5)
 
 	driver.quit()
+
+
+url = "http://www.images.google.com"
+
+execute_js(url, "Matterhorn")
+
